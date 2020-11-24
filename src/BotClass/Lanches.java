5c4ac6aee39ff8f60a07d54711af9134c875cbdd
Lucas <lkslucas100@gmail.com>
2020-11-24 16:08:15 -0300

    alterado

--- src/BotClass/Lanches.java
+++ src/BotClass/Lanches.java
@@ -1,5 +1,44 @@
 package BotClass;
 
 public class Lanches {
-
+	public String nomeLanche;
+	public String descrição;
+	public double preço;
+	public int quantidade;
+	
+	public Lanches(String nomeLanche, String descrição, double preço, int quantidade) {
+		super();
+		this.nomeLanche = nomeLanche;
+		this.descrição = descrição;
+		this.preço = preço;
+		this.quantidade = quantidade;
+	}
+	
+	public String getNomeLanche() {
+		return nomeLanche;
+	}
+	public void setNomeLanche(String nomeLanche) {
+		this.nomeLanche = nomeLanche;
+	}
+	public String getDescrição() {
+		return descrição;
+	}
+	public void setDescrição(String descrição) {
+		this.descrição = descrição;
+	}
+	public double getPreço() {
+		return preço;
+	}
+	public void setPreço(double preço) {
+		this.preço = preço;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
+	
 }
